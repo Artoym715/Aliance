93da37b083a4f1cfb3963f67d6f295d223d76ff8
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-12-19 01:05:55 +0600

    fix navbar scroll

--- js/common.js
+++ js/common.js
@@ -1,1 +1,8 @@
-// $('body').hide()
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+	if (this.scrollY > 1) {
+		navbar.classList.add("navber-light");
+	} else {
+		navbar.classList.remove("navber-light");
+	}
+});
