b46ea4af2a3a2081b5d7d12dabf309fcf8b8e10c
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2024-01-09 05:33:45 +0600

    Раздел с блогом

--- js/common.js
+++ js/common.js
@@ -36,50 +36,6 @@ mMenuToggle.addEventListener("click", (event) => {
 	menu.classList.contains('is-open') ? closeMenu() : openMenu();
 });
 
-const swiperSteps = new Swiper('.steps-slider', {
-	// Optional parameters
-	slidesPerView: 4,
-	spaceBetween: 10,
-	// Navigation arrows
-	navigation: {
-		nextEl: '.steps-button-next',
-		prevEl: '.steps-button-prev',
-	},
-	// Responsive breakpoints
-	breakpoints: {
-		// when window width is >= 320px
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 20,
-		},
-		// when window width is >= 576px
-		425: {
-			slidesPerView: 1,
-			spaceBetween: 20,
-		},
-		// when window width is >= 576px
-		576: {
-			slidesPerView: 2,
-			spaceBetween: 30,
-		},
-		// when window width is >= 768px
-		768: {
-			slidesPerView: 3,
-			spaceBetween: 0,
-		},
-		// when window width is >= 1024px
-		1024: {
-			slidesPerView: 3,
-			spaceBetween: 10,
-		},
-		// when window width is >= 1200px
-		1200: {
-			slidesPerView: 3,
-			spaceBetween: 0,
-		}
-	},
-});
-
 const swiper = new Swiper('.features-slider', {
 	// Optional parameters
 	slidesPerView: 5,
@@ -110,6 +66,83 @@ const swiper = new Swiper('.features-slider', {
 		// when window width is >= 1200px
 		1200: {
 			slidesPerView: 5,
+		}
+	},
+
+});
+
+const swiperSteps = new Swiper('.steps-slider', {
+	// Optional parameters
+	slidesPerView: 4,
+	spaceBetween: 10,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.steps-button-next',
+		prevEl: '.steps-button-prev',
+	},
+	// Responsive breakpoints
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerView: 1,
+		},
+		// when window width is >= 576px
+		425: {
+			slidesPerView: 1,
+		},
+		// when window width is >= 576px
+		576: {
+			slidesPerView: 1,
+		},
+		// when window width is >= 768px
+		768: {
+			slidesPerView: 3,
+		},
+		// when window width is >= 1024px
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		// when window width is >= 1200px
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 0,
+		}
+	},
+});
+
+
+const swiperBlog = new Swiper('.blog-slider', {
+	// Optional parameters
+	slidesPerView: 2,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.blog-button-next',
+		prevEl: '.blog-button-prev',
+	},
+	// Responsive breakpoints
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerView: 1,
+		},
+		// when window width is >= 576px
+		576: {
+			slidesPerView: 1,
+		},
+		// when window width is >= 768px
+		768: {
+			slidesPerView: 1,
+		},
+		// when window width is >= 1024px
+		1024: {
+			slidesPerView: 1,
+		},
+		// when window width is >= 1200px
+		1200: {
+			slidesPerView: 2,
+			spaceBetween: 30
 		}
 	},
 
