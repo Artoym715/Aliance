f9385f2d84ba5ed9ab60da2c440b8a3e55083175
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2024-01-21 00:53:55 +0600

    Обновление модального окна

--- js/common.js
+++ js/common.js
@@ -3,8 +3,7 @@ const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 const modal = document.querySelector(".modal");
-const modalToogle = document.querySelectorAll('[data-toggle="modal"]');
-const modalClose = document.querySelector(".modal-close");
+const modalDialog = document.querySelector(".modal-dilog");
 
 
 const lightModeOn = (event) => {
@@ -152,14 +151,17 @@ const swiperBlog = new Swiper('.blog-slider', {
 
 });
 
-modalToogle.forEach(element => {
-	element.addEventListener("click", (event => {
+document.addEventListener("click", event => {
+	if (
+		event.target.dataset.toggle == "modal" || event.target.parentNode.dataset.toggle == "modal" ||
+		!event.composedPath().includes(modalDialog) && modal.classList.contains("is-open")
+	) {
 		event.preventDefault();
-		modal.classList.add("is-open");
-	}))
+		modal.classList.toggle("is-open");
+	}
 });
-
-modalClose.addEventListener("click", (event => {
-	event.preventDefault();
-	modal.classList.remove("is-open");
-}));
+document.addEventListener("keyup", (event) => {
+	if (event.key == "Escape" && modal.classList.contains("is-open")) {
+		modal.classList.toggle("is-open");
+	}
+});
